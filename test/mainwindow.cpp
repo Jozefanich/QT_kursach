6b0c026a358eecfd3e54a71351a5891564fd64e3
Jozefanich <artem.shabetia@gmail.com>
2025-12-02 21:15:57 +0200

    point 1: create field mxn, pointer contains in QList variable

---
+++ test/mainwindow.cpp
@@ -0,0 +1,36 @@
+﻿#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent){
+    load_field();
+    start_game();
+}
+
+void MainWindow::load_field(){
+    vbl = new QVBoxLayout(this);
+    for(int i=0; i<m; i++){
+        QList<QLabel*> RowItem;
+        QHBoxLayout *hbl = new QHBoxLayout();
+        for(int i=0; i<n; i++){
+            QLabel *cl = new QLabel();
+            cl->setFixedSize(20,20);
+            QString color = "background-color:";
+            color += (((m+n)%2)==0?"green;":"lime;");
+            cl->setStyleSheet(color);
+            RowItem.append(cl);
+            hbl->addWidget(cl);
+        }
+        list.push_back(RowItem);
+        vbl->addLayout(hbl);
+    }
+}
+void MainWindow::start_game(){
+    main = new QWidget(this);
+    setCentralWidget(main);
+    main->setLayout(vbl);
+    main->show();
+    list[0][0]->setStyleSheet("background-color: red;");
+    qDebug() << list[0][0]->styleSheet();
+}
+MainWindow::~MainWindow(){}
---
+++ test/mainwindow.h
@@ -0,0 +1,28 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QhBoxLayout>
+#include <QList>
+#include <QLabel>
+#include <QString>
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+private:
+    int m = 10;
+    int n = 10;
+    int size = 50;
+    QList<QList<QLabel*>> list;
+    QVBoxLayout *vbl = nullptr;
+    // QHBoxLayout *hbl = nullptr;
+    QWidget *main = nullptr;
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    void start_game();
+    void load_field();
+};
+#endif // MAINWINDOW_H
